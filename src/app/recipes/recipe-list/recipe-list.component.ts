85cb9c88ed2af6669cf66060b3eedceeb71d7d77
Peter Greshnov <greshnov.peter@gmail.com>
2021-05-25 16:23:41 +0500

    Recipe list and details adding in progress. Lesson 54

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes=[];
+  recipes: Recipe[] = [
+    new Recipe('Cookies', 'A nice and easy way to bake some Cookies', 'https://ih1.redbubble.net/avatar.2847441.140x140.jpg'),
+    new Recipe('Burgers', 'Beef burger! Yummy!', 'https://realfood.tesco.com/media/images/Burger-31LGH-a296a356-020c-4969-86e8-d8c26139f83f-0-1400x919.jpg')
+  ];
 
   constructor() { }
 

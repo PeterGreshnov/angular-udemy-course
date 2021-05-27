65e8c0a8db8fb650c4ebe0ecf17fedf7ae6a1a56
Peter Greshnov <greshnov.peter@gmail.com>
2021-05-27 18:56:00 +0500

    Lesson 78. All lifecycle hooks added to app-server-element component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [
-    {type: 'server', name: 'Testserver', content: 'Just a test!'},
-    {type: 'blueprint', name: 'Testserver2', content: 'Just a test2!'}];
+    {type: 'server', name: 'Testserver', content: 'Just a test!'}];
 
 
     onServerAdded(serverData: {serverName: string, serverContent:string}) {
@@ -25,5 +24,13 @@ export class AppComponent {
         name: blueprintData.serverName,
         content: blueprintData.serverContent
       });
+    }
+
+    onChangeFirst() {
+      this.serverElements[0].name = 'Changed!';
+    }
+
+    onDestroyFirst() {
+      this.serverElements.splice(0,1);
     }
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  ViewEncapsulation, 
+  OnChanges, 
+  SimpleChange,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,15 +20,56 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+OnInit, 
+OnChanges, 
+DoCheck, 
+AfterContentInit, 
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+OnDestroy {
 
   @Input('srvElement') element: {type: string, name: string, content: string}; 
+  @Input() name: string = ''; 
 
   constructor() {
+    console.log('Constructor called!');
     this.element = {type: '', name: '', content: ''};
    }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
   ngOnInit(): void {
+    console.log('ngOnInit called!');
+  }
+  
+  ngDoCheck() {
+    console.log('ngDoCheck called!');
+  }
+  
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called!');
+
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!');
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked called!');
+
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called!');
   }
   
 }
